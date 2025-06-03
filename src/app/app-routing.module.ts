8c3685aee3fac64ed6fd59083deb9c98d103d8fa
Franco David Gonzalez <francomac@Mac-mini-de-Franco.local>
2025-06-03 15:59:50 -0300

    setup: estructura inicial + routing base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'teams', loadChildren: () => import('./features/teams/teams.module').then(m => m.TeamsModule) }, 
+  { path: 'drivers', loadChildren: () => import('./features/drivers/drivers.module').then(m => m.DriversModule) },
+  { path: 'constructors', loadChildren: () => import('./features/constructors/constructors.module').then(m => m.ConstructorsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/features/constructors/constructors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-constructors',
+  standalone: false,
+  templateUrl: './constructors.component.html',
+  styleUrl: './constructors.component.scss'
+})
+export class ConstructorsComponent {
+
+}
