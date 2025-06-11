d6df1950bb4b1e8630c6f9a68cbccd080f921880
Franco David Gonzalez <francomac@Mac-mini-de-Franco.local>
2025-06-11 11:15:21 -0300

    wip: add transfer state to service f1

--- src/app/core/services/f1-api.service.ts
+++ src/app/core/services/f1-api.service.ts
@@ -1,14 +1,19 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, makeStateKey, PLATFORM_ID, TransferState } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject, finalize, of } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, tap } from 'rxjs/operators';
 
 import { Team, TeamsResponse, Driver, DriversResponse, ChampionshipDriver, DriversChampionshipResponse, ChampionshipConstructor, ConstructorsChampionshipResponse } from '../models/models';
+import { isPlatformServer } from '@angular/common';
 
 @Injectable({ providedIn: 'root' })
 export class F1ApiService {
   private readonly baseUrl = 'https://f1api.dev/api';
   private readonly http = inject(HttpClient);
+
+  // Transfer state
+  private readonly state = inject(TransferState);
+  private readonly platformId = inject(PLATFORM_ID);
 
   // --- Estados de carga ---
   private readonly _isLoadingTeams = new BehaviorSubject<boolean>(false);
@@ -24,41 +29,50 @@ export class F1ApiService {
   isLoadingChampionshipConstructors$ = this._isLoadingChampionshipConstructors.asObservable();
 
 
-  getTeams(year: number): Observable<Team[]> {
-    this._isLoadingTeams.next(true);
-    return this.http.get<TeamsResponse>(`${this.baseUrl}/${year}/teams`).pipe(
-      map(response => this.mapTeams(response.teams)),
-      finalize(() => this._isLoadingTeams.next(false)),
-      catchError(err => {
-        console.error('Error fetching teams in F1ApiService:', err);
-        return of([]);
-      })
-    );
+getTeams(year: number): Observable<Team[]> {
+  return this.fetchWithTransferState(
+    `teams-${year}`,
+    () => this.http.get<TeamsResponse>(`${this.baseUrl}/${year}/teams`).pipe(
+      map(res => this.mapTeams(res.teams))
+    ),
+    this._isLoadingTeams
+  );
+}
+
+getDrivers(year: number): Observable<Driver[]> {
+  return this.fetchWithTransferState(
+    `drivers-${year}`,
+    () => this.http.get<DriversResponse>(`${this.baseUrl}/${year}/drivers`).pipe(
+      map(res => this.mapDrivers(res.drivers))
+    ),
+    this._isLoadingDrivers
+  );
+}
+
+getAllDrivers(limit = 1000, offset = 0): Observable<Driver[]> {
+  const key = makeStateKey<Driver[]>(`drivers-all-${limit}-${offset}`);
+  const fromCache = this.state.get(key, null!);
+
+  if (fromCache) {
+    return of(fromCache);
   }
 
-  getDrivers(year: number): Observable<Driver[]> {
-    this._isLoadingDrivers.next(true);
-    return this.http.get<DriversResponse>(`${this.baseUrl}/${year}/drivers`).pipe(
-      map(response => this.mapDrivers(response.drivers)),
-      finalize(() => this._isLoadingDrivers.next(false)),
-      catchError(err => {
-        console.error('Error fetching drivers in F1ApiService:', err);
-        return of([]);
-      })
-    );
-  }
+  this._isLoadingDrivers.next(true);
+  return this.http.get<DriversResponse>(`${this.baseUrl}/drivers?limit=${limit}&offset=${offset}`).pipe(
+    map(response => this.mapDrivers(response.drivers)),
+    tap(data => {
+      if (isPlatformServer(this.platformId)) {
+        this.state.set(key, data);
+      }
+    }),
+    finalize(() => this._isLoadingDrivers.next(false)),
+    catchError(err => {
+      console.error('Error fetching all drivers in F1ApiService:', err);
+      return of([]);
+    })
+  );
+}
 
-  getAllDrivers(limit = 1000, offset = 0): Observable<Driver[]> {
-    this._isLoadingDrivers.next(true);
-    return this.http.get<DriversResponse>(`${this.baseUrl}/drivers?limit=${limit}&offset=${offset}`).pipe(
-      map(response => this.mapDrivers(response.drivers)),
-      finalize(() => this._isLoadingDrivers.next(false)),
-      catchError(err => {
-        console.error('Error fetching all drivers in F1ApiService:', err);
-        return of([]);
-      })
-    );
-  }
 
   searchDrivers(query: string): Observable<Driver[]> {
     this._isLoadingDrivers.next(true);
@@ -72,29 +86,39 @@ export class F1ApiService {
     );
   }
 
-  getTopDriversByYear(year: number): Observable<ChampionshipDriver[]> {
-    this._isLoadingChampionshipDrivers.next(true);
-    return this.http.get<DriversChampionshipResponse>(`${this.baseUrl}/${year}/drivers-championship`).pipe(
-      map(response => response.drivers_championship),
-      finalize(() => this._isLoadingChampionshipDrivers.next(false)),
-      catchError(err => {
-        console.error('Error fetching top drivers in F1ApiService:', err);
-        return of([]);
-      })
-    );
+getTopDriversByYear(year: number): Observable<ChampionshipDriver[]> {
+  return this.fetchWithTransferState(
+    `drivers-championship-${year}`,
+    () => this.http.get<DriversChampionshipResponse>(`${this.baseUrl}/${year}/drivers-championship`).pipe(
+      map(res => res.drivers_championship)
+    ),
+    this._isLoadingChampionshipDrivers
+  );
+}
+
+getTopConstructorsByYear(year: number): Observable<ChampionshipConstructor[]> {
+  const key = makeStateKey<ChampionshipConstructor[]>(`constructors-championship-${year}`);
+  const fromCache = this.state.get(key, null!);
+
+  if (fromCache) {
+    return of(fromCache);
   }
 
-  getTopConstructorsByYear(year: number): Observable<ChampionshipConstructor[]> {
-    this._isLoadingChampionshipConstructors.next(true);
-    return this.http.get<ConstructorsChampionshipResponse>(`${this.baseUrl}/${year}/constructors-championship`).pipe(
-      map(response => response.constructors_championship),
-      finalize(() => this._isLoadingChampionshipConstructors.next(false)),
-      catchError(err => {
-        console.error('Error fetching top constructors in F1ApiService:', err);
-        return of([]);
-      })
-    );
-  }
+  this._isLoadingChampionshipConstructors.next(true);
+  return this.http.get<ConstructorsChampionshipResponse>(`${this.baseUrl}/${year}/constructors-championship`).pipe(
+    map(response => response.constructors_championship),
+    tap(data => {
+      if (isPlatformServer(this.platformId)) {
+        this.state.set(key, data);
+      }
+    }),
+    finalize(() => this._isLoadingChampionshipConstructors.next(false)),
+    catchError(err => {
+      console.error('Error fetching top constructors in F1ApiService:', err);
+      return of([]);
+    })
+  );
+}
 
   // --- Utilidad interna ---
   private mapDrivers(drivers: Driver[]): Driver[] {
@@ -124,4 +148,30 @@ export class F1ApiService {
     }
     return null;
   }
+
+  //Transfer state => ng state en el codigo fuente
+  private fetchWithTransferState<T>(
+  keyName: string,
+  fetchFn: () => Observable<T>,
+  setLoading?: BehaviorSubject<boolean>
+): Observable<T> {
+  const key = makeStateKey<T>(keyName);
+  const cached = this.state.get(key, null!);
+  if (cached) return of(cached);
+
+  setLoading?.next(true);
+  return fetchFn().pipe(
+    tap(data => {
+      if (isPlatformServer(this.platformId)) {
+        this.state.set(key, data);
+      }
+    }),
+    finalize(() => setLoading?.next(false)),
+    catchError(err => {
+      console.error(`Error fetching ${keyName}:`, err);
+      return of([] as unknown as T);
+    })
+  );
+}
+
 }
